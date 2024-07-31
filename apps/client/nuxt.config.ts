5ab39dd3bd1b17bed2c31c0cd4397134bec5fde0
CharleeWa <18888351756@163.com>
2024-07-31 17:59:38 +0800

    chore: update deps

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   typescript: { shim: false },
   modules: ['@nuxt/ui'],
+  compatibilityDate: '2024-07-31',
 })
--- eslint.config.ts
+++ eslint.config.ts
@@ -3,4 +3,8 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   typescript: true,
   vue: true,
+
+  ignores: [
+    'scripts/**',
+  ],
 })
